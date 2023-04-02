3fcff4fd9ce4f7ac62fc6801538c1291e847b8c8
Salah Bentayeb <bentayebtech@gmail.com>
2023-04-02 02:00:41 +0100

    Added eslint config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
+  darkMode: "media",
   theme: {
     extend: {},
   },
